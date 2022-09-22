de9df9ffca36fca0ec298a927904f2799ac09925
Tom Farley <farleytpm@gmail.com>
2022-09-22 10:19:45 +0100

    Rename template files

--- othona/__meta__.py
+++ othona/__meta__.py
@@ -1,12 +1,12 @@
 # `name` is the name of the package as used for `pip install package`
-name = "package-name"
+name = "othona"
 # `path` is the name of the package for `import package`
 path = name.lower().replace("-", "_").replace(" ", "_")
 # Your version number should follow https://python.org/dev/peps/pep-0440 and
 # https://semver.org
-version = "0.1.dev0"
-author = "Author Name"
-author_email = ""
-description = ""  # One-liner
+version = "2022.9.dev0"
+author = "Tom Farley"
+author_email = "farleytpm@gmail.com"
+description = "Python tools for the Othona Community"  # One-liner
 url = ""  # your project homepage
 license = "MIT"  # See https://choosealicense.com
